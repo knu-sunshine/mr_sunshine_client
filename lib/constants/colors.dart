ad2324b074813efe6efce40a8b6f6f20c26d91ac
HyeonJunSong <music9801@naver.com>
2024-01-12 14:43:07 +0530

    design: color constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color primary = Color(0xFFFDEEB6);
+
+  static const Color surface = Color(0xFFFBFBFB);
+  static const Color surfaceStroke = Color(0xFFF5F5F5);
+
+  static const Color black = Color(0xFF000000);
+}
