60324772bcd3c6a9ac72e5ed9d9308f435d322f8
HyeonJunSong <music9801@naver.com>
2024-01-12 14:41:27 +0530

    config: main file setings

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-void main() {
+import 'view/00_authPage/authPage.dart';
+import 'viewModel/userViewModel.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'mr.sunshine',
-      home: RootPage(),
-    );
-  }
-}
-
-class RootPage extends StatefulWidget {
-  const RootPage({super.key});
-
-  @override
-  State<RootPage> createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+    return ScreenUtilInit(
+        designSize: const Size(393, 852),
+        builder: (context, child) {
+          return GetMaterialApp(
+            title: 'mr.sunshine',
+            theme: ThemeData(
+              fontFamily: "Noto-Sans",
+            ),
+            debugShowCheckedModeBanner: false,
+            home: AuthPage(),
+            initialBinding: BindingsBuilder(() {
+              Get.put(UserController());
+            }),
+            getPages: [
+              GetPage(name: '/auth', page: () => AuthPage()),
+            ],
+          );
+        }
     );
   }
 }
