f30fb1fb916037153b6f270b79c1f288b7009a75
HyeonJunSong <music9801@naver.com>
2024-01-18 09:17:21 +0530

    design: design assets

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primary = Color(0xFFFDEEB6);
 
+  static const Color background = Color(0xFFFFFFFF);
+
   static const Color surface = Color(0xFFFBFBFB);
   static const Color onSurface = Color(0xFFF4F4F4);
   static const Color surfaceStroke = Color(0xFFF5F5F5);
