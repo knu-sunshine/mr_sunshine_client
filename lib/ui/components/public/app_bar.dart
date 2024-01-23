267f23964390851de6f8db1b2cce8a2db31cdee3
HyeonJunSong <music9801@naver.com>
2024-01-23 14:57:25 +0530

    refactor: app bar refactor universially

--- lib/ui/components/public/app_bar.dart
+++ lib/ui/components/public/app_bar.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mr_sunshine_client/constants/colors.dart';
 
-Widget appBar() {
+Widget appBar({required String title, Widget? suffixWidget}) {
   return Container(
     width: 393.w,
     height: 87.h,
     padding: EdgeInsets.only(
-      top: 30.h,
+      top: 36.h,
       left: 28.w,
+      right: 28.w,
     ),
-    child: Text(
-      "Andrew's HOME",
-      style: TextStyle(
-        fontSize: 24.sp,
-        fontWeight: FontWeight.w700,
-        color: AppColor.black,
-      ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 24.sp,
+            fontWeight: FontWeight.w700,
+            color: AppColor.black,
+          ),
+        ),
+        suffixWidget ?? Container(),
+      ],
     ),
   );
 }
