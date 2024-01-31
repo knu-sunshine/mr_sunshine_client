366975d066b0e96081f5348fb1442662fbdf3c1b
HyeonJunSong <music9801@naver.com>
2024-01-31 10:56:39 +0530

    feat: google oauth client id setting

--- lib/network_provider/google_oauth_network_provider.dart
+++ lib/network_provider/google_oauth_network_provider.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleOauth {
   static Future<OAuthCredential> getCredential() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+            clientId: dotenv.env["GOOGLE_CLIENT_ID"],
+            serverClientId: dotenv.env["GOOGLE_CLIENT_ID"])
+        .signIn();
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
 
